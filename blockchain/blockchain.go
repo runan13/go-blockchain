7af675b113a1039ab1e3de6a96bcf74f296479c3
runan13 <tjrrhj123@gmail.com>
2022-08-21 18:54:49 +0900

    Singleton - blockchain package

---
+++ blockchain/blockchain.go
@@ -0,0 +1,58 @@
+package blockchain
+
+import (
+	"crypto/sha256"
+	"fmt"
+	"sync"
+)
+
+type block struct {
+	Data     string
+	Hash     string
+	PrevHash string
+}
+
+type blockchain struct {
+	blocks []*block
+}
+
+var b *blockchain
+var once sync.Once
+
+func (b *block) calculateHash() {
+	hash := sha256.Sum256([]byte(b.Data + b.PrevHash))
+	b.Hash = fmt.Sprintf("%x", hash)
+}
+
+func getLastHash() string {
+	totalBlocks := len(GetBlockchain().blocks)
+	if totalBlocks == 0 {
+		return ""
+	}
+	return GetBlockchain().blocks[totalBlocks-1].Hash
+}
+
+func createBlock(data string) *block {
+	newBlock := block{data, "", getLastHash()}
+	newBlock.calculateHash()
+	return &newBlock
+}
+
+func (b *blockchain) AddBlock(data string) {
+	b.blocks = append(b.blocks, createBlock(data))
+}
+
+func (b *blockchain) AllBlocks() []*block {
+	return b.blocks
+}
+
+// GetBlockchain 프로그램이 병렬로 실행되어도 Blockchain을 가져오는 함수는 단 한번만 실행되도록 sync package를 사용
+func GetBlockchain() *blockchain {
+	if b == nil {
+		once.Do(func() {
+			b = &blockchain{}
+			b.AddBlock("Genesis Block")
+		})
+	}
+	return b
+}
